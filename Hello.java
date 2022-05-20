c4043d7d435c55e39d77527bce4be4b1b6ff6426
cetc28 <zhaoxt@nuaa.edu.cn>
2022-05-20 14:40:33 +0800

    master branch modify

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
              public static void main(String[] args ){
-           System out println("Hello git init")；
-          System out println("modyfy by cetc28")；
+           System out println("hello git init")；
+          System out println("modyfy by cetc28 !!!")；
           System out println("modyfy by Dev branch")；
+          System out println("modyfy by master branch")；
 }
 }
